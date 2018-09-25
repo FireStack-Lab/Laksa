7ee955e2039f25612a64b9e988c45f0b4aba9a97
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-25 06:22:03 +0000

    Deploy FireStack-Lab/Laksa to github.com/FireStack-Lab/Laksa.git:gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@ const del = require('del')
 
 const packages = [
   'laksa-core-crypto',
-  'laksa-request',
+  'laksa-core-messenger',
+  'laksa-providers-http',
   'laksa-shared',
   'laksa-utils',
+  'laksa-core-methods',
+  'laksa-core-properties',
+  'laksa-wallet',
+  'laksa-hd-wallet',
   'laksa-zil',
   'laksa-core',
   'laksa'
