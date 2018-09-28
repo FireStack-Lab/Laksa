0e90e43625c7725eb0015393ad7104739e802960
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-28 09:53:09 +0000

    Deploy FireStack-Lab/Laksa to github.com/FireStack-Lab/Laksa.git:gh-pages

--- packages/laksa-core-crypto/lib/index.js
+++ packages/laksa-core-crypto/lib/index.js
@@ -351,27 +351,7 @@ var toChecksumAddress = function toChecksumAddress(address) {
  */
 
 var isValidChecksumAddress = function isValidChecksumAddress(address) {
-  return isAddress(address.replace('0x', '')) && toChecksumAddress(address) === address;
-};
-/**
- * verify if param is correct
- * @param  {[hex|string]}  address [description]
- * @return {Boolean}         [description]
- */
-// const isAddress = (address) => {
-//   return !!address.match(/^[0-9a-fA-F]{40}$/)
-// }
-
-var isAddress = function isAddress(address) {
-  if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
-    // check if it has the basic requirements of an address
-    return false;
-  } else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
-    // If it's all small caps or all all caps, return true
-    return true;
-  }
-
-  return false;
+  return toChecksumAddress(address) === address;
 };
 
 exports.randomBytes = randomBytes;
