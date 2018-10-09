071a0b25984538fcea4720ae0e9c09f716f3045e
neeboo <ghostcorn@gmail.com>
2018-10-09 12:24:02 +0800

    >	deleted:    packages/laksa-core-contract/lib/index.js >	deleted:    packages/laksa-core-contract/node/index.js >	deleted:    packages/laksa-core-crypto/lib/index.js >	deleted:    packages/laksa-core-crypto/node/index.js >	deleted:    packages/laksa-core-messenger/lib/index.js >	deleted:    packages/laksa-core-messenger/node/index.js >	deleted:    packages/laksa-core-methods/lib/index.js >	deleted:    packages/laksa-core-methods/node/index.js >	deleted:    packages/laksa-core-properties/lib/index.js >	deleted:    packages/laksa-core-properties/node/index.js >	deleted:    packages/laksa-core/lib/index.js >	deleted:    packages/laksa-core/node/index.js >	deleted:    packages/laksa-extend-keystore/lib/index.js >	deleted:    packages/laksa-extend-keystore/node/index.js >	deleted:    packages/laksa-hd-wallet/lib/index.js >	deleted:    packages/laksa-hd-wallet/node/index.js >	deleted:    packages/laksa-providers-http/lib/index.js >	deleted:    packages/laksa-providers-http/node/index.js >	deleted:    packages/laksa-shared/lib/index.js >	deleted:    packages/laksa-shared/node/index.js >	deleted:    packages/laksa-utils/lib/index.js >	deleted:    packages/laksa-utils/node/index.js >	deleted:    packages/laksa-wallet/lib/index.js >	deleted:    packages/laksa-wallet/node/index.js >	deleted:    packages/laksa-zil/lib/index.js >	deleted:    packages/laksa-zil/node/index.js >	deleted:    packages/laksa/lib/index.js >	deleted:    packages/laksa/node/index.js

--- packages/laksa/lib/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Laksa = require('laksa-core');
-
-if (typeof window !== 'undefined' && typeof window.Laksa === 'undefined') {
-  window.Laksa = Laksa;
-}
-
-module.exports = Laksa;
