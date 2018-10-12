2bf4b15115d323bbabe3e2cf796f92288222a1c3
neeboo <ghostcorn@gmail.com>
2018-10-12 22:02:51 +0800

    modify randomBytes

--- packages/laksa-core-crypto/src/random.js
+++ packages/laksa-core-crypto/src/random.js
@@ -10,16 +10,16 @@
 import RB from 'randomBytes'
 
 export const randomBytes = bytes => {
-  let randBz
+  const randBz = RB(bytes)
 
-  if (typeof window !== 'undefined' && window.crypto && window.crypto.getRandomValues) {
-    randBz = window.crypto.getRandomValues(new Uint8Array(bytes))
-  } else if (typeof require !== 'undefined') {
-    // randBz = require('crypto').randomBytes(bytes)
-    randBz = RB(bytes)
-  } else {
-    throw new Error('Unable to generate safe random numbers.')
-  }
+  // if (typeof window !== 'undefined' && window.crypto && window.crypto.getRandomValues) {
+  //   randBz = window.crypto.getRandomValues(new Uint8Array(bytes))
+  // } else if (typeof require !== 'undefined') {
+  //   // randBz = require('crypto').randomBytes(bytes)
+  //   randBz = RB(bytes)
+  // } else {
+  //   throw new Error('Unable to generate safe random numbers.')
+  // }
 
   let randStr = ''
   for (let i = 0; i < bytes; i += 1) {
