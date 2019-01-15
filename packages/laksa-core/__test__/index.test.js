51d8c88d5a774463f18b2d7e170ea62ca54eaf05
neeboo <ghostcorn@gmail.com>
2019-01-15 20:56:07 +0800

    update test to latest chain_ID

--- packages/laksa-core/__test__/index.test.js
+++ packages/laksa-core/__test__/index.test.js
@@ -35,6 +35,6 @@ describe('test Laksa', () => {
       pkg: laksa.Modules.HttpProvider
     })
     expect(lll.currentProvider.node.url).toEqual('http://localhost:4200')
-    expect(lll.getNetworkSetting().TestNet.CHAIN_ID).toEqual(2)
+    expect(lll.getNetworkSetting().TestNet.CHAIN_ID).toEqual(62)
   })
 })
